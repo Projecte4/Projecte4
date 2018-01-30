32083247e31b40996d5e3c93d566b355da94e9dd
David <martinezpunzanodavid96@gmail.com>
2018-01-30 17:28:10 +0100

    Mejora de css mas codigo mas implementacion de mapas

---
+++ mycontacts2/mostrarContactos.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include"conexion_proc.php";
+$idusu=$_SESSION['id'];
+$q = "SELECT * FROM contactos WHERE id_usu='$idusu'";
+$resultado = mysqli_query($conexion, $q);
+if (mysqli_num_rows($resultado)>0) {
+    while ($registro=mysqli_fetch_array($resultado)) {
+      $idCon=$registro['id_contacto'];
+      echo "<div class='contactos'>";
+      echo "<br/>Nombre: $registro[nombre]";
+      echo "<br/>Apellido/Apellidos: $registro[apellidos]";
+      echo "<br/>Correo: $registro[correo]";
+      echo "<br/>Direccion: $registro[direccion]";
+      echo "<br/>Telefono: $registro[telefono]";
+      echo "</div>";
+    }
+}
+
+ ?>
---
+++ mycontacts2/registro.proc.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		$nombre=$_REQUEST['Nombre'];
+		$apellido=$_REQUEST['Apellido'];
+		$correo=$_REQUEST['Correo'];
+		$usuario=$_REQUEST['Usuario'];
+		$pass=$_REQUEST['Password'];
+		$pass2=$_REQUEST['Password2'];
+		$conexion = mysqli_connect ("localhost", "root", "","bd_mycontacts");
+		$sqlNom="SELECT * FROM usuarios WHERE nick='$usuario'";
+		$consultaNom=mysqli_query($conexion,$sqlNom);
+		$rowcount=mysqli_num_rows($consultaNom);
+		if($rowcount==0){
+			if ($pass==$pass2) {
+				$sql="INSERT INTO `usuarios`(`id_usu`, `nombre_usu`, `apellidos_usu`, `correo`, `nick`, `password`) VALUES (null, '$nombre', '$apellido', $correo, '$usuario', '$pass')";
+			$consulta=mysqli_query($conexion,$sql);
+			echo "<a>¡Bienvenido a nuestra pagina!</a>";
+			 echo"<form method=post action=indexs.php>";
+	    	echo"<center><input type=submit value=Volver></center>";
+	    	echo "</form>";
+			}else{
+				echo"<a>Usuario o contraseña incorrectos. Vuelva a intentarlo</a>";
+				echo"<form method=post action=indexs.php>";
+	    	echo"<center><input type=submit value=Volver></center>";
+	    	echo "</form>";
+			}
+		}else {
+			echo "<a>Nick ya usado,Introduzca otro.</a>";
+			echo"<form method=post action=indexs.php>";
+			echo"<center><input type=submit value=Volver></center>";
+			echo "</form>";
+		}
+?>
+
+
+
+</body>
+</html>
